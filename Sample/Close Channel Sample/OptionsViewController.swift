007f6097c66b8f6821ec301dba0ffafc55214c96
Maurice de Bijl <maurice@debijl.net>
2022-06-29 12:01:44 +0200

    Using SDK version 1.0.2 and showing the version in the Sample app

--- Sample/Close Channel Sample/OptionsViewController.swift
+++ Sample/Close Channel Sample/OptionsViewController.swift
@@ -6,13 +6,24 @@
 //
 
 import UIKit
+import CloseChannel
 
 class OptionsViewController: UIViewController {
+    let closeChannelController = CloseChannelController.sharedInstance
 
     var newChannelTap: (() -> Void)?
     var openLastChannelMessagesTap: (() -> Void)?
     var openLastChannelInfoTap: (() -> Void)?
     var registerUserTap: (() -> Void)?
+
+    @IBOutlet var sdkVersionLabel: UILabel!
+
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+
+        sdkVersionLabel.textColor = .black
+        sdkVersionLabel.text = "🔋 Powered by Close\nMobile Close Channel SDK \(closeChannelController.version)"
+    }
 
     @IBAction func newChannelTapped() {
         newChannelTap?()
