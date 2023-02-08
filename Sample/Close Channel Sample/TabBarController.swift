2679bbb2118bef51e7ae6a2eeada722c920cd4c4
Maurice <maurice@thecloseapp.com>
2023-02-08 13:45:27 +0100

    Updated Sample app to show a navigationbar for the messages view and some cleaning up

--- Sample/Close Channel Sample/TabBarController.swift
+++ Sample/Close Channel Sample/TabBarController.swift
@@ -8,21 +8,6 @@
 import UIKit
 import CloseChannel
 
-extension UIViewController {
-    /**
-     *  Height of status bar + navigation bar (if navigation bar exist)
-     */
-    var topBarHeight: CGFloat {
-        var top = self.navigationController?.navigationBar.frame.height ?? 0.0
-        if #available(iOS 13.0, *) {
-            top += UIApplication.shared.windows.first?.windowScene?.statusBarManager?.statusBarFrame.height ?? 0
-        } else {
-            top += UIApplication.shared.statusBarFrame.height
-        }
-        return top
-    }
-}
-
 class TabBarController: UITabBarController {
 
     let closeChannelController = CloseChannelController.sharedInstance
@@ -195,7 +180,7 @@ class TabBarController: UITabBarController {
         optionsViewController?.showChannelViewControllerTap = {
             self.closeChannelController.getChannelMessagesViewController(channelId: nil) { channelMessagesViewController in
                 channelMessagesViewController.title = "Talk to us"
-
+                let navigationBar = UINavigationController(rootViewController: channelMessagesViewController)
                 self.showChannelViewController(channelMessagesViewController)
                 self.viewControllers?.last?.tabBarItem = UITabBarItem(title: "Messages", image: UIImage(named: "messages"), selectedImage: nil)
 
